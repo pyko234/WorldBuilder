be58ef5bbd31b609ab39dc5d88ed3d7c9244cac2
pyko234 <sonnen.david@gmail.com>
2024-03-03 11:51:59 -0500

    Added Documentation to the WorldBuilderApp class

--- gui.py
+++ gui.py
@@ -1602,26 +1602,55 @@ class EditEntryFrame(tk.Frame):
 
 
 class WorldBuilderApp(tk.Tk):
+    """
+    Main application class for the World Builder App.
+
+    This class represents the main application window and handles the creation of frames, menu bar, and window events.
+    """
+
     def __init__(self):
+        """
+        Initializes the WorldBuilderApp.
+
+        This method sets up the main application window, initializes application data, creates the menu bar,
+        initializes frame classes, and sets the initial frame to display. It also binds the closing event to the
+        on_closing method.
+        """
+
+        # Set self to a a tk.Tk
         tk.Tk.__init__(self)
+
+        # Set the title for the window
         self.title("World Builder App")
 
+        # Instantiate the app data class
         self.app_data = AppData()
 
         # Create the menu bar
         self.create_menu_bar()
 
+        # Create list of frame classes
         frame_classes = [WorldSelectionFrame, WorldOverviewFrame, NewEntrySelectCategoryFrame, ViewEntryFrame, EditEntryFrame]
 
+        # Create dictionary of frames with the names as the key and no instance of the class saved
         self.frames = {frame_class: None for frame_class in frame_classes}
-        self.current_frame = None  # Track the current frame
 
+        # Track the current frame
+        self.current_frame = None
+
+        # Show frame WorldSelectionFrame
         self.show_frame(WorldSelectionFrame)
 
         # Bind the closing event to the on_closing method
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
     def create_menu_bar(self):
+        """
+        Creates the menu bar for the application.
+
+        This method creates a menu bar with a file menu that includes an option to exit the application.
+        """
+
         # Create a menu bar
         menubar = tk.Menu(self)
 
@@ -1634,15 +1663,43 @@ class WorldBuilderApp(tk.Tk):
         self.config(menu=menubar)
 
     def on_closing(self):
+        """
+        Handles the closing event of the application window.
+
+        This method prompts the user to confirm if they want to quit the application. If confirmed, it closes the
+        database session (if exists) and destroys the main application window.
+        """
+
+        # Run messagebox to ask for if the user wants to quit
         if messagebox.askokcancel("Quit", "Do you want to quit?"):
+
+            # Check for session
             if self.app_data.session:
+
+                # Close session
                 self.app_data.session.close()
+            
+            # Destroy main window
             self.destroy()
 
     def show_frame(self, cont):
+        """
+        Displays the specified frame in the main application window.
+
+        Args:
+            cont: The class or instance of the frame to be displayed.
+
+        This method displays the specified frame in the main application window, hides the current frame (if exists),
+        updates the current frame, and adjusts the window size accordingly. Additionally, it sets specific dimensions
+        for frames of type EditEntryFrame or ViewEntryFrame.
+        """
+
+        # Check for instance of frame
         if isinstance(cont, tk.Frame):
+
             # If an instance of the frame is provided, use it directly
             frame = cont
+
         else:
             # If a frame class is provided, create a new instance
             frame = cont(self, self, self.app_data)
@@ -1661,6 +1718,7 @@ class WorldBuilderApp(tk.Tk):
         # Update the current frame
         self.current_frame = frame
 
+        # If the frame has the update_label_text method
         if hasattr(frame, "update_label_text"):
             frame.update_label_text()
 
